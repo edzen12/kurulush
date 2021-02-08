ac896a5bd050f8237b1522444847f5b932d69554
edzen12 <oichiev.edzen@gmail.com>
2021-02-09 01:19:42 +0600

    add host

--- kurulush/settings.py
+++ kurulush/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'k*4n7=@a40ho!1&tkla-3yg3^i9*wxflw9%o&sl^+nsiv*d5&&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'depkur.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000', '188.225.36.187']
 # ALLOWED_HOSTS = []
 
 
