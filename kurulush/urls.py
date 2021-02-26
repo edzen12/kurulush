2b05fafafea65477d6b0ca46cdc4dce487dbc9a5
edzen12 <oichiev.edzen@gmail.com>
2021-02-26 13:56:34 +0600

    fix settings py

--- kurulush/urls.py
+++ kurulush/urls.py
@@ -22,9 +22,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls')),
-    path('', include('apps.news.urls')),
-    path('', include('apps.gallery.urls')),
-    path('', include('apps.product.urls')),
+    path('news/', include('apps.news.urls')),
+    path('gallery/', include('apps.gallery.urls')),
+    path('product/', include('apps.product.urls')),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
