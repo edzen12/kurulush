3982d57e286583e75e784562b24d1041ecf5b304
edzen12 <oichiev.edzen@gmail.com>
2021-03-31 21:16:35 +0600

    add app google smtp

--- kurulush/settings.py
+++ kurulush/settings.py
@@ -174,4 +174,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'oichiev.edzen@gmail.com'
-EMAIL_HOST_PASSWORD = 'xzxyaexaenfqkbgy'
+EMAIL_HOST_PASSWORD = 'zgyyjvgrjtbdkfrv'
